69097c05db62d2cd56f2ac4c13b6c6990becf320
Ajarek <ajarek@poczta.onet.pl>
2022-11-21 01:03:26 +0100

    added data.json

---
+++ src/components/CardProject/CardProject.tsx
@@ -0,0 +1,29 @@
+import './CardProject.css'
+export type Props = {
+  path: string
+  src: string
+  title: string
+  technology: string
+  id: number
+}
+
+const CardProject = ({ path, src, title, technology, id,  }: Props) => {
+  return (
+    <div
+      className='card-project'
+      key={id}
+    >
+      <a href={path} target='_blanc' >
+        <img
+          src={src}
+          alt={title}
+          
+        />
+      </a>
+      <h2>{title}</h2>
+      <p>{technology}</p>
+    </div>
+  )
+}
+
+export default CardProject
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import CardProject from '../../components/CardProject/CardProject'
 import './Projects.css'
+import {Props} from '../../components/CardProject/CardProject'
 export const Projects = () => {
+  const [images, setImages] = useState([])
+ 
+  useEffect(() => {
+    const fetchDataJSON=async()=> {
+      const response = await fetch('/data/dataProjects.json')
+      setImages(await response.json())
+      
+    }
+    fetchDataJSON()
+  },[])
+  console.log(images); 
   return (
     <div className='projects'>
       <h1>Projects</h1>
-      <div className="projects-wrapper">
-        
+      <div className='projects-wrapper'>
+        {images&&images.map((el:Props)=><CardProject 
+        key={el.id}
+        src={el.src}
+        path={el.path}
+        title={el.title}
+        technology={el.technology}
+        id={el.id}
+
+        />)}
       </div>
-      </div>
+    </div>
   )
 }
 
