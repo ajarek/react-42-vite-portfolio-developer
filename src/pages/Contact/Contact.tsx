3d18db0c27135262bf38e9ba2d3f04773fa7c2c1
Ajarek <ajarek@poczta.onet.pl>
2022-11-23 22:25:54 +0100

    bug fix src all

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -22,7 +22,7 @@ export const Contact = () => {
         >
           <div className='logo'>
             <img
-              src='/public/my-logo.png'
+              src='public/my-logo.png'
               alt='logo'
             />
             @jarek
